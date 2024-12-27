5250107ff542fc1d9541d172f970d6f162117953
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-27 17:45:15 +0900

    Update app.sql

--- db/app.sql
+++ db/app.sql
@@ -1,7 +1,8 @@
-create schema `app`;
-create database `app`;
-
+CREATE SCHEMA IF NOT EXISTS `app`;
 use app;
+
+CREATE DATABASE IF NOT EXISTS `app`;
+
 create table if not exists student
 (
     id   int auto_increment
