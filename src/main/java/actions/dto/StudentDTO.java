1874357d8c9df8b62a53f87a9a82ebeb9f6e29e3
leGit-y <99312658+leGit-y@users.noreply.github.com>
2024-12-30 16:25:25 +0900

    [feat] create student

--- src/main/java/actions/dto/StudentDTO.java
+++ src/main/java/actions/dto/StudentDTO.java
@@ -3,19 +3,31 @@ package actions.dto;
 import actions.entity.Student;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@AllArgsConstructor
+
 @ToString
 @Getter
 public class StudentDTO {
 
     private String name;
 
+    protected StudentDTO(){}
+
+    private StudentDTO(String name){
+        this.name = name;
+    }
+
+    public static StudentDTO of(String name){
+        return new StudentDTO(name);
+    }
     public static StudentDTO from (Student student){
-        return new StudentDTO(
-                student.getName()
-        );
+        return StudentDTO.of(student.getName());
+    }
+
+    public static Student toEntity (StudentDTO studentDTO){
+        return Student.of(studentDTO.getName());
     }
 
 }
